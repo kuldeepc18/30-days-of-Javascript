7bd83b305e87dc9256cec192205124f5a097bee8
kuldeepc18 <choudharykuldeep1803@gmail.com>
2024-07-25 22:04:23 +0530

    files updated

--- Day13_(Modules)/Activity 5 - Module Bundling (Optional)/Task 8/src/index.js
+++ Day13_(Modules)/Activity 5 - Module Bundling (Optional)/Task 8/src/index.js
@@ -1,11 +1,3 @@
-/**
- * Day 13 - Modules
- * 
- * Activity 5 - Module Bundling (Optional)
- *  - Task 8: Use a module bundler like Webpack or Parcel to bundle multiple JavaScript files into a single file. Write a script to demonstrate the bundling process.
- */
-
-
 // Import the greet function from the greetModule
 const greet = require('./greetModule');
 
